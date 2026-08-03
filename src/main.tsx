a0bea2e609fb0a50d553ae2494595fbb9a2d1b3b
flintpaybank-sudo <flint.paybank@gmail.com>
2026-08-03 08:32:38 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import './index.css';
+import './App.css'; // Remplacez App.css par le nom exact de votre fichier
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
