1de748962683783ea1f8803716bed35c058b3204
flintpaybank-sudo <flint.paybank@gmail.com>
2026-08-03 03:53:10 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from '../App' // On ajoute ../ pour remonter d'un dossier vers la racine
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
