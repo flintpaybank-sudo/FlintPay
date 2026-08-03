c5f9ae21c9e8b847a9818181f5e61927d3ccbc0d
flintpaybank-sudo <flint.paybank@gmail.com>
2026-08-03 04:02:39 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Bienvenue sur FlintPay !</h1>
+      <p>L'application est correctement déployée et connectée.</p>
+    </div>
+  )
+}
